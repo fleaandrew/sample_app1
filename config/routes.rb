22b5200fc181c111456a855667e865c5f4678828
fleaandrew <andr_papuhsa@ukr.net>
2016-07-16 00:49:52 +0300

    Finish User sign up

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   get 'help'    => 'static_pages#help'
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
+  post 'signup' => 'users#create'
+  resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
